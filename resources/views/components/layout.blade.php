418968ad2e0f69f1550ae03bb00d87019ee03362
Darken <zerboinoussas4@gmail.com>
2024-02-04 09:04:56 +0000

    a

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -17,11 +17,12 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     </head>
-    <body class="font-sans antialiased">
+    <body class="font-sans antialiased h-screen justify-center" style="background: #edf2f7;">
         @include('shared._navbar')
 
-        <div class=" container px-6">
-            <!-- Page Content -->
+
+
+
             <!-- flash info -->
             @if (session('success'))
                         
